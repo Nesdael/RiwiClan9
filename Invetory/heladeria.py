5a7192659932d3ba72039af0dc1e8ed900675a14
Daniel Duran <78667699+Nesdael@users.noreply.github.com>
2026-03-10 20:49:22 -0500

    Add files via upload

---
+++ Invetory/heladeria.py
@@ -0,0 +1,30 @@
+vainilla = 0
+chocolate = 0
+fresa = 0
+ventas_realizadas = 0
+
+print("heladeria popeye")
+print("1: vainilla")
+print("2: chocolate")
+print("3: fresa")
+
+while ventas_realizadas < 5:
+    try:
+        sabor= int(input("Que tipo de helado quieres? \n"))
+        if sabor == 1:
+            vainilla += 1
+            ventas_realizadas += 1
+        elif sabor == 2:
+            chocolate += 1
+            ventas_realizadas += 1
+        elif sabor == 3:
+            fresa += 1
+            ventas_realizadas += 1
+        else:
+            print("esa opcion no esta") 
+    except ValueError:
+        print("Ingrese un valor correcto")
+        
+print(f"EL sabor de vainilla fue pedido {vainilla}")    
+print(f"EL sabor de vainilla fue pedido {chocolate}")  
+print(f"EL sabor de vainilla fue pedido {fresa}")  
