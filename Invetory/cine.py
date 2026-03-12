231cf5c1ad5cbd39b9fbc13b67eed4e6363e6004
Daniel Duran <78667699+Nesdael@users.noreply.github.com>
2026-03-11 20:51:26 -0500

    Delete Invetory directory

--- Invetory/cine.py
+++
@@ -1,9 +0,0 @@
-edad = int(input("Que edad tienes?\n"))
-
-if edad < 12:
-    print(f"Tienes {edad} años y debes pagar $8.000")
-elif 12 >= edad <= 59:
-    print(f"Tienes {edad} años y debes pagar $12.000")  
-else:
-    print (f"Tienes {edad} años y debes pagar $9.000")
-    
